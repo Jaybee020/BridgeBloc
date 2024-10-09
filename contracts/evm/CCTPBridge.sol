bcdbe8e18d10d8a64fbbb4d68600d730335b3d39
Jaybee020 <69249201+Jaybee020@users.noreply.github.com>
2024-10-09 01:24:46 +0100

    Update CCTPBridge.sol

--- contracts/evm/CCTPBridge.sol
+++ contracts/evm/CCTPBridge.sol
@@ -16,13 +16,9 @@ import "./libraries/BridgeHelper.sol";
 contract CrossChainBridge is Ownable, BridgeUtil {
     using SafeERC20 for IERC20;
 
-    enum ChainType {
-        EVM,
-        Solana
-    }
+    enum ChainType { EVM, Solana }
 
     struct DestinationChain {
-        uint32 domainId;
         ChainType chainType;
         bool isSupported;
     }
@@ -37,33 +33,17 @@ contract CrossChainBridge is Ownable, BridgeUtil {
     ITokenMessenger public immutable tokenMessenger;
     IMessageTransmitter public immutable messageTransmitter;
 
-
     mapping(uint32 => DestinationChain) public supportedDestinationChains;
     mapping(uint32 => mapping(bytes32 => bool)) public supportedDestinationTokens;
 
-    event BridgeDepositReceived(
-        address indexed from,
-        bytes32 indexed recipient,
-        uint32 sourceChain,
-        uint32 destinationChain,
-        uint64 nonce,
-        uint256 amount,
-        address sourceToken,
-        bytes32 destinationToken
-    );
-    event BridgeWithdrawalMade(
-        address indexed recipient,
-        uint64 indexed nonce,
-        uint256 amount,
-        address indexed token
-    );
+    event BridgeDepositReceived(address indexed from, bytes32 indexed recipient, uint32 sourceChain, uint32 destinationChain, uint64 nonce, uint256 amount, address sourceToken, bytes32 destinationToken);
+    event BridgeWithdrawalMade(address indexed recipient, uint64 indexed nonce, uint256 amount, address indexed token);
     event DestinationChainAdded(uint32 indexed domainId, ChainType chainType);
     event DestinationChainRemoved(uint32 indexed domainId);
     event DestinationTokenAdded(uint32 indexed domainId, bytes32 indexed token);
     event DestinationTokenRemoved(uint32 indexed domainId, bytes32 indexed token);
 
-
-constructor(
+    constructor(
         address swapRouterAddr,
         address usdcTokenAddr,
         address tokenMessengerAddr,
@@ -90,21 +70,17 @@ constructor(
         CCTP_DOMAIN = domain;
         WETH_USDC_SWAP_FEE = WETH_USDC_SWAP_FEE_;
 
-        // Initialize supported destination chains
         for (uint256 i = 0; i < destinationDomains.length; i++) {
             _addDestinationChain(destinationDomains[i], chainTypes[i]);
         }
 
-        // Initialize supported destination tokens
         for (uint256 i = 0; i < tokenDomains.length; i++) {
             _addDestinationToken(tokenDomains[i], destinationTokens[i]);
         }
     }
 
-
     function checkPoolExists(address tokenA, address tokenB, uint24 fee) internal view returns (bool) {
-        address poolAddr = IUniswapV3Factory(swapFactory).getPool(tokenA, tokenB, fee);
-        return poolAddr != address(0);
+        return IUniswapV3Factory(swapFactory).getPool(tokenA, tokenB, fee) != address(0);
     }
 
     function performSwap(
@@ -124,10 +100,8 @@ constructor(
         IERC20(tokenA).safeApprove(address(swapRouter), amount);
 
         if (checkPoolExists(tokenA, tokenB, fee)) {
-            // Direct swap
             amountOut = executeSwap(tokenA, tokenB, _recipient, fee, amount);
         } else if (checkPoolExists(tokenA, WETH, fee) && checkPoolExists(WETH, tokenB, fee)) {
-            // Two-step swap via WETH
             uint256 wethAmount = executeSwap(tokenA, WETH, address(this), fee, amount);
             IERC20(WETH).safeApprove(address(swapRouter), wethAmount);
             amountOut = executeSwap(WETH, tokenB, _recipient, fee, wethAmount);
@@ -148,17 +122,16 @@ constructor(
         uint24 fee,
         uint256 amountIn
     ) internal returns (uint256 amountOut) {
-         IAvaxSwapRouter.ExactInputSingleParams memory params = IAvaxSwapRouter.ExactInputSingleParams({
-                tokenIn: tokenIn,
-                tokenOut: tokenOut,
-                fee: fee,
-                recipient: recipient,
-                amountIn: amountIn,
-                amountOutMinimum: 0,
-                sqrtPriceLimitX96: 0
-            });
-            amountOut = avaxSwapRouter.exactInputSingle(params);
-      
+        IAvaxSwapRouter.ExactInputSingleParams memory params = IAvaxSwapRouter.ExactInputSingleParams({
+            tokenIn: tokenIn,
+            tokenOut: tokenOut,
+            fee: fee,
+            recipient: recipient,
+            amountIn: amountIn,
+            amountOutMinimum: 0,
+            sqrtPriceLimitX96: 0
+        });
+        amountOut = avaxSwapRouter.exactInputSingle(params);
     }
 
     function isSupportedToken(address token, uint24 paymentTokenSwapFee) public view returns (bool) {
@@ -169,10 +142,6 @@ constructor(
                (checkPoolExists(tokenA, WETH, paymentTokenSwapFee) && checkPoolExists(WETH, tokenB, paymentTokenSwapFee));
     }
 
-
-
-//TO-DO
-//how do I not use approve and transferFrom?
     function deposit(
         uint256 amount,
         address sourceToken,
@@ -182,16 +151,18 @@ constructor(
         bytes32 recipient,
         bytes32 destinationContract
     ) external payable returns (uint64) {
-        require( sourceToken == address(usdcToken) ||isSupportedToken(sourceToken, fee), "Source Token not supported");
-        require(supportedDestinationChains[destinationDomain].isSupported, "Destination chain not supported");//is this neccesary?
+        require(sourceToken == address(usdcToken) || isSupportedToken(sourceToken, fee), "Source Token not supported");
+        require(supportedDestinationChains[destinationDomain].isSupported, "Destination chain not supported");
         require(supportedDestinationTokens[destinationDomain][destinationToken], "Destination token not supported for this chain");
-        if(sourceToken == address(0)) {
+        
+        if (sourceToken == address(0)) {
             require(msg.value == amount, "Incorrect amount of ether");
-        }else{
-            IERC20(sourceToken).safeTransferFrom(msg.sender, address(this), amount);// try to do without approval
+        } else {
+            IERC20(sourceToken).safeTransferFrom(msg.sender, address(this), amount);
         }
+        
         uint256 amountOut = sourceToken != address(usdcToken)
-            ? performSwap(sourceToken, address(usdcToken), address(this),fee ,amount)
+            ? performSwap(sourceToken, address(usdcToken), address(this), fee, amount)
             : amount;
 
         usdcToken.safeApprove(address(tokenMessenger), amountOut);
@@ -215,8 +186,35 @@ constructor(
         return nonce;
     }
 
+    function sendToRecipient(
+        bytes calldata message,
+        bytes calldata signature,
+        uint64 nonce,
+        uint256 amount,
+        address destinationToken,
+        address recipientAddress,
+        uint24 fee
+    ) external onlyOwner {
+        require(messageTransmitter.receiveMessage(message, signature), "Receive Message Failed");
+        
+        uint256 amountOut;
+        if (destinationToken == address(usdcToken)) {
+            usdcToken.safeTransfer(recipientAddress, amount);
+            amountOut = amount;
+        } else {
+            amountOut = performSwap(address(usdcToken), destinationToken, address(this), fee, amount);
+            if (destinationToken == address(0)) {
+                recipientAddress.call{value: amountOut}("");
+            } else {
+                IERC20(destinationToken).safeTransfer(recipientAddress, amountOut);
+            }
+        }
+
+        emit BridgeWithdrawalMade(recipientAddress, nonce, amountOut, destinationToken);
+    }
+
     function _addDestinationChain(uint32 domainId, ChainType chainType) internal {
-        supportedDestinationChains[domainId] = DestinationChain(domainId, chainType, true);
+        supportedDestinationChains[domainId] = DestinationChain(chainType, true);
         emit DestinationChainAdded(domainId, chainType);
     }
 
@@ -228,51 +226,23 @@ constructor(
         delete supportedDestinationChains[domainId];
         emit DestinationChainRemoved(domainId);
     }
+
     function _addDestinationToken(uint32 domainId, bytes32 token) internal {  
         require(supportedDestinationChains[domainId].isSupported, "Destination chain not supported");
         supportedDestinationTokens[domainId][token] = true;
         emit DestinationTokenAdded(domainId, token);
-      }
+    }
 
     function addDestinationToken(uint32 domainId, bytes32 token) external onlyOwner {
         _addDestinationToken(domainId, token);
     }
 
     function removeDestinationToken(uint32 domainId, bytes32 token) external onlyOwner {
-         supportedDestinationTokens[domainId][token] = false;
+        supportedDestinationTokens[domainId][token] = false;
         emit DestinationTokenRemoved(domainId, token);
-    }
-
-    function sendToRecipient(
-        bytes calldata message,
-        bytes calldata signature,
-        uint64 nonce,
-        uint256 amount,
-        address destinationToken,
-        address recipientAddress,
-        uint24 fee
-    ) external onlyOwner {
-        require(messageTransmitter.receiveMessage(message, signature), "Receive Message Failed");
-        uint256 amountOut;
-        if (destinationToken != address(usdcToken) ) {
-
-            if (destinationToken == address(0)) {
-                amountOut = performSwap(address(usdcToken), destinationToken, address(this), fee,amount);
-                IWETH(WETH).withdraw(amountOut);
-                recipientAddress.call{value: amountOut}("");
-            }else{
-                amountOut = performSwap(address(usdcToken), destinationToken, recipientAddress, fee,amount);
-            }
-        } else {
-               usdcToken.safeTransfer(recipientAddress, amount);
-               amountOut = amount;
-        }
-
-        emit BridgeWithdrawalMade(recipientAddress, nonce, amountOut, destinationToken);
     }
 
     function withdraw(uint256 amount) external onlyOwner {
         usdcToken.safeTransfer(msg.sender, amount);
     }
-
 }
